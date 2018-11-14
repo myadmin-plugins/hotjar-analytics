eba240213cb9add8d3adb3823345cf991d319d1e
Joe Huss <detain@interserver.net>
2018-11-13 22:00:42 -0500

    localization update

--- src/Plugin.php
+++ src/Plugin.php
@@ -42,9 +42,8 @@ class Plugin
 		$menu = $event->getSubject();
 		if ($GLOBALS['tf']->ima == 'admin') {
 			function_requirements('has_acl');
-			if (has_acl('client_billing')) {
-				$menu->add_link('admin', 'choice=none.abuse_admin', '/lib/webhostinghub-glyphs-icons/icons/development-16/Black/icon-spam.png', __('Hotjar'));
-			}
+            if (has_acl('client_billing')) {
+            }
 		}
 	}
 
@@ -72,7 +71,5 @@ class Plugin
          * @var \MyAdmin\Settings $settings
          **/
         $settings = $event->getSubject();
-		$settings->add_text_setting(__('General'), __('Hotjar'), 'abuse_imap_user', __('Hotjar IMAP User'), __('Hotjar IMAP Username'), ABUSE_IMAP_USER);
-		$settings->add_text_setting(__('General'), __('Hotjar'), 'abuse_imap_pass', __('Hotjar IMAP Pass'), __('Hotjar IMAP Password'), ABUSE_IMAP_PASS);
-	}
+    }
 }
